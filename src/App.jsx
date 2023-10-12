960514d5d0fc5e81f177ab4ce19754ae62ddf43f
Timon Hosch <timon.hosch@gmail.com>
2023-10-12 10:02:38 +0200

    Eventlisteners intergriert

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles/styles.css";
 
 function App() {
   const [player, setPlayer] = useState("");
-  const [field, setField] = useState("")
+  const [fields, setFields] = useState()
 
-  const fields = []
-  for (let i = 0; i < 9; i++) {
-    fields.push(<div key={crypto.randomUUID()}></div>)
+  const handleClick = (e) => {
+    e.target.innerText = player
+    if (e.target.innerText !== "") return
+    if (player === "X") {setPlayer("O")}
+    else {setPlayer("X")}
   }
 
   return (
     <>
       <div className="players">
         <h1>select your symbol:</h1>
-        <button value="0" onClick={(e) => setPlayer(e.target.value)}>
+        <button value="O" onClick={(e) => setPlayer(e.target.value)}>
           O
         </button>
         <button value="X" onClick={(e) => setPlayer(e.target.value)}>
@@ -22,7 +24,15 @@ function App() {
         </button>
       </div>
       <div className="grid">
-        {fields}
+        <div onClick={e => handleClick(e)}></div>
+        <div onClick={e => handleClick(e)}></div>
+        <div onClick={e => handleClick(e)}></div>
+        <div onClick={e => handleClick(e)}></div>
+        <div onClick={e => handleClick(e)}></div>
+        <div onClick={e => handleClick(e)}></div>
+        <div onClick={e => handleClick(e)}></div>
+        <div onClick={e => handleClick(e)}></div>
+        <div onClick={e => handleClick(e)}></div>
       </div>
     </>
   );
